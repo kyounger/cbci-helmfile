31ec2496280511f9f33fd273dba1fec0fdbe2bed
Kenny Younger <kyounger@gmail.com>
2020-07-02 11:27:33 -0500

    Idempotently create token

--- values/groovy/oc-custom-groovy/create-k8s-secret-with-jenkins-token.groovy
+++ values/groovy/oc-custom-groovy/create-k8s-secret-with-jenkins-token.groovy
@@ -9,6 +9,15 @@ def namespace = "cloudbees"
 
 def user = User.get(userName, false)
 def apiTokenProperty = user.getProperty(ApiTokenProperty.class)
+def tokens = apiTokenProperty.tokenStore.getTokenListSortedByName().findAll {it.name==jenkinsTokenName}
+
+if(tokens.size() != 0) {
+    println("Token exists. Revoking any with this name and recreating to ensure we have a valid value stored in the secret.")
+    tokens.each {
+        apiTokenProperty.tokenStore.revokeToken(it.getUuid())
+    }
+}
+
 def result = apiTokenProperty.tokenStore.generateNewToken(jenkinsTokenName).plainValue
 user.save()
 
